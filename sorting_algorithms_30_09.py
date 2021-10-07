57c6b1d97b9d8701252ad6e574bafb5fd0ea2086
SomSomDani <50622778+SomSomDani@users.noreply.github.com>
2021-10-07 07:50:25 +0200

    Adding sorting_algorithms
    adding bubbleSort
    adding selectionSort
    adding insertionSort

---
+++ sorting_algorithms_30_09.py
@@ -0,0 +1,41 @@
+import random;
+def bubblesort(mlist):
+    n = len(mlist)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if mlist[j] > mlist[j + 1]:
+                mlist[j], mlist[j + 1] = mlist[j + 1], mlist[j]
+    return mlist
+
+
+def insertionsort(mlist):
+    for i in range(1, len(mlist)):
+        key = mlist[i]
+        j = i - 1
+        while j >= 0 and key < mlist[j]:
+            mlist[j + 1] = mlist[j]
+            j -= 1
+        mlist[j + 1] = key
+    return mlist
+
+
+def selectionsort(mlist):
+    for i in range(len(mlist)):
+        min_idx = i
+        for j in range(i + 1, len(mlist)):
+            if mlist[min_idx] > mlist[j]:
+                min_idx = j
+        mlist[i], mlist[min_idx] = mlist[min_idx], mlist[i]
+    return mlist
+
+if __name__ == "__main__":
+        nlist = []
+        for j in range(0, 100):
+            n = random.randint(0,100)
+            nlist.append(n)
+        bsortlist = bubblesort(nlist)
+        isortlist = insertionsort(nlist)
+        ssortlist = selectionsort(nlist)
+        print(bsortlist)
+        print(isortlist)
+        print(ssortlist)
